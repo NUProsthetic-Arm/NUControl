c9508d91f1c74e832459653ce83e6043384f82bf
Mjenz <michael.jenz77@gmail.com>
2026-04-07 16:01:11 -0500

    removed old trajectory files

--- src/swing_traj_segments.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-
-extern std::vector<float> trajectory_traj_segment_traj_05_swing_47;
-extern std::vector<float> trajectory_traj_segment_traj_07_swing_6;
-extern std::vector<float> trajectory_traj_segment_traj_09_swing_1;
-extern std::vector<float> trajectory_traj_segment_traj_11_swing_70;
-extern std::vector<float> trajectory_traj_segment_traj_13_swing_27;
-extern std::vector<float> trajectory_traj_segment_traj_15_swing_15;
